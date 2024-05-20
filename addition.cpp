e84f6c4bd116e4f3e52abaf2e2b8ed50e45c01b9
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-05-20 22:02:49 +0530

    addition of two number

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter any two number that you want to add"<<endl;
+    cin>>a>>b;
+
+    int c;
+    c = a + b;
+    cout<<"Addition of the given number is  ==> "<< c <<endl;
+
+    return 0;
+
+}
