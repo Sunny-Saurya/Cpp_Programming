a433a0a54fbf3664c15e0f96ac3a69c6fd34ad74
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-05-24 22:01:57 +0530

    added function file

---
+++ function.cpp
@@ -0,0 +1,68 @@
+// Function is piece of code that performs a specific task.
+//How to write functions.
+// returnType functionName(parameter1,parameter2.....parameterN)
+
+// write a code of addition by using FUNCTION
+
+// #include<iostream>
+// using namespace std;
+
+// int add(int num1, int num2){
+//     int sum = num1 + num2;
+//     return sum;
+// }
+
+// int main(){
+//     int a,b;
+//     a = 3;
+//     b = 78;
+//     cout<<add(a,b)<<endl;
+//     return 0;
+// }
+
+//Print all prime numbers between 2 given number.
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// bool isPrime(int num)
+// {
+//     for(int i = 2; i<=sqrt(num); i++){
+//         if(num%i == 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// int main(){
+//     int a,b ;
+//     cin>>a>>b;
+
+//     for(int i = a; i<=b; i++)
+//     {
+//         if(isPrime(i)){
+//             cout<<i<<endl;
+//         }
+//     }
+//     return 0;
+// // }
+
+// #include<iostream>
+// using namespace std;
+
+// int add(int num1, int num2){
+//     int sum = num1 + num2;
+//     return sum;
+// }
+
+// int main(){
+//     int a,b;
+//     cin>>a>>b;
+
+//     a = 9;
+//     b = 80;
+//     cout<<add(a,b)<<endl;
+//     return 0;
+// }
+
