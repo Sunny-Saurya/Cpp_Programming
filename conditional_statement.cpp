a3e6a3b6c2f2270a4c879711c5848e8c8757e431
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-02 05:54:54 +0530

    conditional_statement

---
+++ conditional_statement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 12;
+    int n2 = 43;
+    if(n>n2)
+    {
+        cout<<n <<" is the greatest number"<<endl;
+    }
+    else{
+        cout<<n2 <<" is the greatest number"<<endl;
+    }
+}
