2d1f0e3d830fc88621fca8f549792f0920894a9d
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-02 07:10:53 +0530

    pattern question 2

---
+++ pattern_2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row,col;
+    cout<<"Enter the number of rows: ";
+    cin>>row;
+    cout<<"Enter the number of colums: ";
+    cin>>col;
+    
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;  
+    }
+    
+}
