08b46cca71b08a9493c222fc86f0284081e8c75f
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-02 06:02:25 +0530

    greatest number

---
+++ greatest_number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    cin>>num1;
+    int num2;
+    cin>>num2;
+
+    if(num1>num2) cout<<num1<<" is the greater number"<<endl;
+    else cout<<num2<<" is the greater number"<<endl;
+}
