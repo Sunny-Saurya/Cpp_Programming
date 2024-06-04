68ba015922a6d08162cba011e681114fda6b171c
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-05 00:36:17 +0530

    bitwise operator

---
+++ bitwize_operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 4;
+    int b = 5;
+
+    cout<<"a&b=> "<< (a&b) <<endl;
+    cout<<"a&b=> "<< (a|b)<<endl;
+    cout<<"~a=> "<< (~a)<<endl;
+    cout<<"~b=> "<< (~b)<<endl;
+    cout<<"a^b => "<< (a^b)<<endl;
+    cout<<"b^a => "<< (b^a)<<endl;
+}
