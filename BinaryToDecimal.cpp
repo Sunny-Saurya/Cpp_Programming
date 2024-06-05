15140a06bb2f7abf712fc07a47ab9c6a7422a92c
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-05 11:41:21 +0530

    Binary to Decimal Conversion

--- BinaryToDecimal.cpp
+++ BinaryToDecimal.cpp
@@ -16,8 +16,8 @@ int main()
     {
         int bit = n & 1;
         ans = (bit * pow( 10 , i)) + ans;
-        n = n>>1;
+        n = n >> 1;
         i++;
     }
-    cout<<"Answer is "<< ans<<endl;
+    cout<<"Answer is "<< ans <<endl;
 }
