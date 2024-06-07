952b05e975c21b4dd2180e6bbd9f428532a9098d
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-07 11:42:44 +0530

    Binary Search Program

---
+++ ArrayCpp/Binary_Search.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int arr [], int n, int key)
+{
+    int start = 0;
+    int end = n - 1;
+
+    int mid = ( start + end ) / 2;
+
+    while( start <= end )
+    {
+        if( key == arr[mid] )
+        {
+            return mid;
+        }
+        if(key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = ( start + end ) / 2;
+
+    }
+    return -1;
+} 
+int main()
+{
+    int n;
+    cout<< "Enter the size of an array: "<< endl;
+    cin>>n;
+
+    int arr[100];
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "The given Array is : "<< " ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    int key;
+    cout << "Enter any key that you want to seach : " << " ";
+    cin >> key;
+
+    int index = BinarySearch( arr , n , key);
+    cout<<"The index of key " << key << " is : " << index << " ";
+
+}
