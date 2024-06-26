bac03d2cb67f9da39da21cff604b11b5c1751087
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-26 10:48:33 +0530

    rotation element

---
+++ ArrayCpp/SelectionSort/rotatedArray.cpp
@@ -0,0 +1,44 @@
+// Rotated Array with the position of k
+
+#include<iostream>
+using namespace std;
+
+int rotatedArray(int arr[], int n, int k)
+{
+    for(int i = k + 1; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    for(int i = 0; i <= k; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter size : " << " ";
+    cin >> n;
+
+    int arr[100];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "The array you entered is : " << " ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int k;
+    cout << "Enter the index you want to rotate the array from : " << " ";
+    cin >> k;
+
+    cout << "The array after rotation " << " ";
+    rotatedArray(arr, n, k);
+
+}
