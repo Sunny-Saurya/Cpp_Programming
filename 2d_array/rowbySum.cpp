a19df1628417c37e4857247d90f9b1a01dd2e4a5
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-30 22:44:46 +0530

    row wise sum

---
+++ 2d_array/rowbySum.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int rowWiseSum(int arr[][20], int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < m; j++)
+        {
+            sum += arr[i][j];
+        }
+        cout << "sum is : " << sum << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter number of rows : " << " ";
+    cin >> n;
+
+    int m;
+    cout << "Enter number of columns : " << " ";
+    cin >> m;
+
+    int arr[n][20];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    rowWiseSum(arr, n, m);
+    // cout << "Sum of all elements in the array is : " << sum1 << endl;
+}
