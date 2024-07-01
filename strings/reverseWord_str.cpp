2d46f71c2aea012dc83b7b3e76d6f701b5aa1bb0
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-07-01 16:37:37 +0530

    basic of cpp

---
+++ strings/reverseWord_str.cpp
@@ -0,0 +1,46 @@
+// // my name is sunny ===> ym eman si ynnus
+
+// #include <iostream>
+// #include<string>
+// using namespace std;
+
+
+// int getLenght(char name[]) // not need of lenght
+// {
+//     int sum = 0;
+
+//     for(int i = 0; name[i] != '\0'; i++)
+//     {
+//         sum++;
+//     }
+//     return sum;
+// }
+
+// char reverseWord(char  name[], int sum){
+//     char rev[100];
+
+//     for(int i = 0; name[i] != '\0'; i++)
+//     {
+//         if(name[i] = ' ')
+//         {
+//             name[i] = rev[i];
+//             i++;
+//         }
+//         else if(name[i] = '\0')
+//         {
+//             name[i] = rev[i];
+//             i++;
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     string name;
+//     cout << "Enter your name: " << " ";
+//     cin >> name;
+//     cout << "Hello, " << name << "!" << endl;
+
+//     int len = getLenght(name);
+//     cout << reverseWord(name,len) << " ";
+// }
