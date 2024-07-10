82201917f3b1f7fbfb2d99c9e5b64363cec88d6c
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-07-10 23:38:53 +0530

    counting

--- Recussion/counting.cpp
+++ Recussion/counting.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 void couting(int n)
 {
+    // base case
     if(n == 0)
     {
         return;
     }
+
+    //recussive function
     cout << n << " ";
     couting(n-1);
+
+    // series wise
+    // cout << n << " ";
+    // couting(n-1);
 }
 
 int main()
