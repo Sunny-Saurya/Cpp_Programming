1d8cc1afc6558267f78e75223d2472a70d7c4a1c
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-08-01 17:25:59 +0530

    insert at head

---
+++ LinkedList/video44/insertAtHead.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    
+    public:
+    int data;
+    Node* next;
+
+    // constructor
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+
+};
+
+void InsertAtHead(Node* &head, int d)
+{
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+};
+
+void print(Node *head){
+    Node* temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // node
+    Node* node1 = new Node(10);
+
+    // cout << node1 -> data << endl;  // 0
+    // cout << node1 -> next << endl;  // garbage value
+
+    Node* head = node1;
+    print(head);
+
+    InsertAtHead(head, 12);
+    print(head);
+
+    InsertAtHead(head, 15);
+    print(head);
+}
