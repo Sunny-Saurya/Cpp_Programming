a273b821b80b6526432260a493a9661abd3c9bc3
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-08-01 17:13:53 +0530

    introduction to Linked List

---
+++ LinkedList/video44/intro.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    
+    public:
+    int data;
+    Node* next;
+
+    // constructor
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+
+};
+
+int main()
+{
+    // node
+    Node* node1 = new Node(10);
+
+    cout << node1 -> data << endl;  // 0
+    cout << node1 -> next << endl;  // garbage value
+}
