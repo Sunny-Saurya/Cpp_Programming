0cb0ffd5db23b357c988108f526c43658cd61c10
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-08-21 19:28:11 +0530

    Sturct, union in c++

---
+++ structureUnionEnum.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+// user defined datatype ---> which is make by different different data type
+
+// typedef struct employee
+// {
+//     int emp_Id;
+//     char favChar;
+//     float salary;
+// }ep;
+
+// union datatype
+
+// union money  // used for better memeory manangement
+// {
+//     int rice;
+//     char car;
+//     int modelNumber;
+// };
+
+
+int main()
+{
+
+    // STRUCTURE --------------
+
+
+    // struct employee sunny;
+    // ep sanya;
+
+    // sanya.emp_Id = 2;
+    // sanya.favChar = 'S';
+    // sanya.salary = 5000;
+
+    // sunny.emp_Id = 1;
+    // sunny.favChar = 'S';
+    // sunny.salary = 12399;
+
+
+    // cout << sunny.favChar << endl;
+    // cout << sunny.salary << endl;
+    // cout << sunny.emp_Id << endl;
+    // cout <<"Sanya's data : " << endl;
+    // cout << sanya.favChar << endl;
+    // cout << sanya.salary << endl;
+    // cout << sanya.emp_Id << endl;
+
+    // UNION -------------
+
+    // union money m1;
+    // m1.rice = 34;
+    // m1.car = 'c';
+
+    // cout << m1.rice << endl;
+
+    return 0;
+}
