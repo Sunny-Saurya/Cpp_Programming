990d2d46c1892f7a41a6ca66eb987dfb29a17249
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-09-08 16:25:39 +0530

    Inorder traversal

---
+++ BinaryTree/inOrderTraversal.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildTree(Node* root) {
+
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new Node(data);    
+
+    if(data == -1) {
+        return NULL;
+    }
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+    
+    return root;
+
+}
+
+
+
+void levelOrderTraversal(Node* root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) { 
+            //purana level complete traverse ho chuka hai
+            cout << endl;
+            if(!q.empty()) { 
+                //queue still has some child ndoes
+                q.push(NULL);
+            }  
+        }
+        else{
+            cout << temp -> data << " ";
+            if(temp ->left) {
+                q.push(temp ->left);
+            }
+
+            if(temp ->right) {
+                q.push(temp ->right);
+            }
+        }
+    }
+
+}
+
+void  inorder(Node* root){
+    // base case
+
+    if(root == NULL){
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+
+int main()
+{
+    Node *root = NULL;
+
+    // Build the tree
+    root = buildTree(root);
+
+    // Perform level order traversal
+    levelOrderTraversal(root);
+
+    //  Perform inorder traversal
+    cout <<  "\nInorder traversal: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
