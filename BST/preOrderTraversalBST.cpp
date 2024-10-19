c871f24352a4055f38b2b61f3acee3c232c76a7f
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-10-19 16:12:15 +0530

    preorder of BST

---
+++ BST/preOrderTraversalBST.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data)
+    {
+        this -> data = data;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+Node* insertBST(Node* root, int n)
+{
+    if(root == NULL) return new Node(n);
+
+    if(root -> data > n)
+    {
+        root -> left = insertBST(root -> left , n);
+    }
+    else{
+        root -> right = insertBST(root -> right , n);
+    }
+
+    return root;
+}
+
+void preOrder(Node* root)
+{
+    if(root == NULL) return ;
+    cout << root -> data << " ";
+    preOrder(root -> left);
+    preOrder(root -> right);
+}
+
+int main()
+{
+    Node* root = NULL;
+    int n;
+
+    while(cin >> n && n != -1)
+    {
+        root = insertBST(root, n);
+    }
+
+    preOrder(root);
+    cout << endl;
+
+    return 0;
+}
