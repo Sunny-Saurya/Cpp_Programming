4c1f72ddd0dbda55bfd6697eb5a23dbfd63abb12
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-10-21 20:36:47 +0530

    sum of left child

---
+++ BST/sumOfLeftChild.cpp
@@ -0,0 +1,116 @@
+//********************** sum of left child *****************************
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int data)
+    {
+        this -> data = data;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+Node* insertBST(Node* root, int value)
+{
+    if(root == NULL) return new Node(value);
+    if(root -> data > value)
+    {
+        root -> left = insertBST(root -> left, value);
+    }
+    else{
+        root -> right = insertBST(root -> right , value);
+    }
+    
+    return root;
+}
+
+
+// void inorder(Node* root)
+// {
+//     int sum = 0;
+//     if(root == NULL ) return;
+//     sum = sum + inorder(root -> left) + inorder(root -> right);
+//     cout << sum << endl;
+// }
+
+
+int sumOfLeftChild(Node* root)
+{
+    if(root == NULL) return 0;
+    
+    int sum = 0;
+    
+    if(root -> left != NULL) sum = sum + root -> left -> data;
+    
+    sum = sum + sumOfLeftChild(root -> left);
+    sum = sum + sumOfLeftChild(root -> right);
+    
+    return sum;
+}
+
+int sumOfRightChild(Node* root)
+{
+    if(root == NULL) return 0;
+    
+    int sum = 0;
+    
+    if(root -> right != NULL) sum = sum + root -> right -> data;
+    
+    sum = sum + sumOfRightChild(root -> left);
+    sum = sum + sumOfRightChild(root -> right);
+    
+    return sum;
+}
+// void preorder(Node* root)
+// {
+//     if(root == NULL) return;
+//     cout << root -> data << " ";
+//     preorder(root -> left);
+//     preorder(root -> right);
+// }
+
+int main()
+{
+    int value;
+    Node* root = NULL;
+    
+    while(cin >> value)
+    {
+        if(value == -1) break;
+        root = insertBST(root, value);
+    }
+    
+    // inorder(root);
+    // preorder(root);
+    
+    cout << "sum of left children is " << sumOfLeftChild(root);
+    cout << "sum of right children is " << sumOfRightChild(root);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
