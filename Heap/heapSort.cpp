e2717d11ee9abf785f617d83293d443e5f86014c
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-10-26 16:43:01 +0530

    heap sort

---
+++ Heap/heapSort.cpp
@@ -0,0 +1,159 @@
+ #include<bits/stdc++.h>
+using namespace std;
+
+class heap{
+    public:
+    int arr[100];
+    int size = 0;
+
+
+    heap()
+    {
+        arr[0] =  -1;
+        size = 0;
+    }
+
+    void insert(int val)
+    {
+
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+
+        while(index > 1)
+        {
+            int parent = index/2;
+            if(arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else
+            {
+                return ;
+            }
+        }
+    }
+
+    void print()
+    {
+        for(int i = 1; i <= size; i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout << endl;
+    }
+
+
+    void deleteHeap()
+    {
+        if(size == 0)
+        {
+            cout << "Nothing to delete" << endl;
+            return;
+        }
+
+        // step1 : put last element into the first index;
+
+        arr[1] = arr[size];
+
+        // step2 : remove the last element
+
+        size--;
+
+        // take root node to its correct position
+
+        int i = 1;
+        
+        while(i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = 2 * i + 1;
+
+            if(leftIndex < size && arr[i] < arr[leftIndex])
+            {
+                swap(arr[i], arr[leftIndex]);
+                i = leftIndex;
+            }
+
+            else if(rightIndex < size && arr[i] < arr[rightIndex])
+            {
+                swap(arr[i], arr[rightIndex]);
+                i = rightIndex;
+            }
+            else{
+                return;
+            }
+        }
+
+    }
+};
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if(left <= n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+    if(right <= n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+    if(largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+
+}
+
+void heapSort(int arr[], int n)
+{
+    int size = n;
+
+    while(size > 1)
+    {
+        // step1  : swap;
+        swap(arr[size], arr[1]);
+        size--;
+
+        heapify(arr, size,1);
+    }
+}
+
+int main()
+{
+    heap h;
+    h.insert(10);
+    h.insert(20);
+    h.insert(15);
+    h.insert(30);
+    h.insert(25);
+    h.print();
+    h.deleteHeap();
+    h.print();
+
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+
+    int n = 5;
+    
+    // heap creation
+    for(int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    cout << "Printing the array now " << endl;
+
+    heapSort(arr, n);
+    for(int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
