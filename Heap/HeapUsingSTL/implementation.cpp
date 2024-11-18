dfa3bbbc0348bceb313cf3deb17b6c91519ddaec
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-11-18 21:45:56 +0530

    implementation of heap using stl

---
+++ Heap/HeapUsingSTL/implementation.cpp
@@ -0,0 +1,30 @@
+// implementation of max heap
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void buildHeap(vector<int> &arr, int n)
+{
+    make_heap(arr.begin(), arr.end());
+
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    buildHeap(arr, n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
