75cbb9f3092e5c2155dfd2793c4b4cce223a1540
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-11-26 22:00:23 +0530

    binary Tree Tilt

---
+++ LeetCode_Question/563_binaryTreeTilt.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+
+    private:
+    int solve(TreeNode* root, int &sum)
+    {
+        if(root == NULL) return 0;
+
+        int left = solve(root -> left, sum);
+        int right = solve(root -> right, sum);
+
+        sum = sum + abs(left - right);
+        return left + right + root -> val;
+    }
+public:
+    int findTilt(TreeNode* root) {
+        int sum = 0;
+        solve(root, sum);
+
+        return sum;
+    }
+};
