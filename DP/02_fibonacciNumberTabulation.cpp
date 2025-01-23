544ad7949ef9adccf86e94a51ad5430eb0336fe7
Sunny Kumar <sunnysaurya9955@gmail.com>
2025-01-23 22:32:58 +0530

    Fibonacci Number Tabulation

---
+++ DP/02_fibonacciNumberTabulation.cpp
@@ -0,0 +1,19 @@
+// # Approach 2 : Bottom -Up Dynamic Programming
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << " Enter the number : ";
+    cin >> n;
+    vector<int> dp(n+1, -1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i = 2; i <= n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    cout << " The number of ways to climb the stairs is : " << dp[n];
+    
+}
