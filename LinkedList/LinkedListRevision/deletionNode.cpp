ff9d2d5a320d906a355541a600d57ca6fb52b296
Sunny Kumar <sunnysaurya9955@gmail.com>
2025-05-19 00:50:47 +0530

    Deletion node in Singly LL

---
+++ LinkedList/LinkedListRevision/deletionNode.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void insertAtHead(Node* &head, int data){
+    Node* newNode = new Node(data);
+    newNode -> next = head;
+    head = newNode;
+}
+
+void insertAtTail(Node* &tail , int data){
+    Node* newNode = new Node(data);
+    tail -> next = newNode;
+    tail = newNode; 
+}
+
+void insertAtPosition(Node* head, Node* tail, int pos,  int data){
+    
+    if(pos == 1){
+        insertAtHead(head, data);
+        return;
+    }
+
+    int count = 1;
+    Node* temp = head;
+
+    while(count < pos -1){
+        temp = temp -> next;
+        count++;
+    }
+
+    if(temp -> next == NULL){
+        insertAtTail(tail, data);
+        return;
+    }
+
+    Node* newNode = new Node(data);
+    newNode -> next = temp -> next;
+    temp -> next = newNode;
+}
+
+void deleteNode(Node* &head, Node* &tail,int pos){
+
+    if(pos == 1){
+        // for first element
+        Node* temp = head;
+        head = head -> next;
+        delete temp;
+    }
+
+    else{
+        // for middle element
+        int count = 1;
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* temp = head;
+
+        while(count < pos ){
+            prev = curr;
+            curr = curr -> next;
+            count++;
+        }
+        prev -> next = curr -> next;
+        if(curr -> next == NULL){
+            tail = prev;
+        }
+        delete curr;
+
+    }
+
+
+
+}
+
+void printNode(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+int main(){
+
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    Node* tail = node1;
+    insertAtHead(head, 12);
+    insertAtHead(head, 14);
+    insertAtHead(head, 16);
+
+    insertAtTail(tail, 18);
+    insertAtTail(tail, 19);
+    insertAtTail(tail, 20);
+
+    insertAtPosition(head, tail, 3, 1000);
+
+    cout << "Before deletion : ";
+    printNode(head);
+    deleteNode(head,tail, 8);
+    cout << "After  deletion : ";
+    printNode(head);
+
+    cout << "The head : " << head -> data << endl;
+    cout << "The tail : " << tail -> data << endl;
+}
