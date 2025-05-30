6a27e727553e791fb11b24aed06f1cb5e7144c98
Sunny Kumar <sunnysaurya9955@gmail.com>
2025-05-30 12:01:13 +0530

    Height of Tree

---
+++ BinarySearchTree/heightOfTree.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this -> data = data;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+void buildFromLevelOrder(Node* &root){
+    queue<Node*> q;
+
+    cout << "Enter data for root : ";
+    int data;
+    cin >> data;
+
+    root = new Node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        cout << "Enter the left data of " << temp -> data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if(leftData != -1){
+            temp -> left = new Node(leftData);
+            q.push(temp -> left);
+        }
+
+        cout << "Enter the right data of " << temp -> data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if(rightData != -1){
+            temp -> right = new Node(rightData);
+            q.push(temp -> right);
+        }
+    }
+}
+
+void levelOrderTraversal(Node* root){
+    queue<Node*>q;
+    q.push(root);
+    q.push(NULL);
+
+    
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL){
+            cout << endl;
+
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout << temp -> data << " ";
+            if(temp -> left){
+                q.push(temp -> left);
+            }
+            if(temp -> right){
+                q.push(temp -> right);
+            }
+        }
+
+        
+    }
+}
+
+int heightOfTree(Node* root){
+    if(root == NULL) return 0;
+
+    int left = heightOfTree(root -> left);
+    int right = heightOfTree(root -> right);
+
+    return max(left , right) + 1;
+}
+
+
+int main(){
+    Node* root = NULL;
+    buildFromLevelOrder(root);
+    levelOrderTraversal(root);
+
+    cout << "The height of tree is : " << heightOfTree(root) << endl;
+}
