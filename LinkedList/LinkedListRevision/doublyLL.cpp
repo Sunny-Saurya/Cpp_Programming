1855181e9d74c786805e5dbfac053f8cfe7a33eb
Sunny Kumar <sunnysaurya9955@gmail.com>
2025-10-17 18:18:30 +0530

    Double LL Implementation

--- LinkedList/LinkedListRevision/doublyLL.cpp
+++ LinkedList/LinkedListRevision/doublyLL.cpp
@@ -92,7 +92,7 @@ void printList(Node* head){
     Node* temp = head;
 
     while(temp != NULL){
-        cout << temp -> data << " ";
+        cout << temp -> data << "<-->";
         temp = temp -> next;
     }
     cout << endl;
