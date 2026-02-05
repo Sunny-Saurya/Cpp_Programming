adce6cdba8e7911e8768fb93cf930156ac442b90
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-02-05 19:22:14 +0530

    Frogs Jumps Memoizatino

--- Dynamic_Programming/04_FrogJumps.cpp
+++ Dynamic_Programming/04_FrogJumps.cpp
@@ -15,10 +15,32 @@ int minimumJumps(vector<int>& arr, int index){
     return min(left, right);
 }
 
+// Memoization
+
+int minimumJumpsMemo(vector<int>& arr, int index, vector<int>&dp){
+    if(index == 0) return 0;
+
+    if(dp[index] != -1) return dp[index];
+
+    int left = minimumJumps(arr, index-1) 
+               + abs(arr[index] - arr[index-1]);
+
+    int right = INT_MAX;
+    if(index > 1)
+        right = minimumJumps(arr, index-2) 
+                + abs(arr[index] - arr[index-2]);
+
+    dp[index] = min(left, right);
+    return dp[index];
+}
+
 int main(){
     int n = 4;
     vector<int> arr = {20, 30, 40, 20};
 
+    // cout << "The Minimum Energy Required to Jump is : "
+    //      << minimumJumps(arr, n-1) << endl;
+    vector<int>dp(n+1, -1);
     cout << "The Minimum Energy Required to Jump is : "
-         << minimumJumps(arr, n-1) << endl;
+         << minimumJumpsMemo(arr, n-1, dp) << endl;
 }
