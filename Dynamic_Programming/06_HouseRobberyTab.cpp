9c3e3638d766a32823804421b99e969065e9e98a
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-02-07 23:07:46 +0530

    House Rebbery

--- Dynamic_Programming/06_HouseRobberyTab.cpp
+++ Dynamic_Programming/06_HouseRobberyTab.cpp
@@ -36,13 +36,36 @@ class Solution {
         return dp[n - 1];
     }
 
+    // space optimization
+
+    int solveSpace(vector<int>&nums){
+        int n = nums.size();
+        int prev1 = nums[0];
+        int prev2 = 0;
+
+        for(int i = 1; i < n; i++){
+            int pick = nums[i];
+            if(i > 1){
+                pick += prev2;
+            }
+
+            int notPick = 0 + prev1;
+
+            int curr = max(pick, notPick);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
 
 public:
     int rob(vector<int>& nums) {
         int index = nums.size();
         // return solve(nums,index-1);
         vector<int> dp(nums.size() + 1, -1);
-        return solveTab(nums);
+        // return solveTab(nums);
+        return solveSpace(nums);
+
     }
 };
 
